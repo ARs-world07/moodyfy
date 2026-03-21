d5e0430e6f4460756036cc43456d7cb6438048b9
Akash Dwivedi <akashdwivedi22102002@gmail.com>
2026-03-21 06:41:22 +0530

    implimented redis for blacklisting token

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 const userModel = require("../models/user.model")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
+const blacklistModel = require("../models/blacklist.model")
+const redis = require("../config/cache")
 
 async function registerUser(req, res){
     const {username, email, password} = req.body
@@ -51,7 +53,7 @@ async function loginUser(req, res){
             {username},
             {email}
         ]
-    })
+    }).select("+password")
 
     if(!user){
         return res.status(400).json({
@@ -85,4 +87,25 @@ async function loginUser(req, res){
 
 }
 
-module.exports = {registerUser, loginUser}
+async function getMe(req, res){
+    const user = await userModel.findById(req.user.id)
+
+    res.status(200).json({
+        message:"user fetched successfully✅",
+        user
+    })
+}
+
+async function logoutUser(req,res){
+    const token = req.cookies.token
+
+    res.clearCookie("token")
+
+    await redis.set( token, Date.now().toString(), "EX" , 60 * 60)
+
+    res.status(200).json({
+        message:"logut successfully"
+    })
+}
+
+module.exports = {registerUser, loginUser, getMe, logoutUser}
